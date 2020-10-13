cf48117bb7ea37ba5efa0d010de458c47cbff5d2
Richard Randell <richrdrandell2@outlook.com>
2020-10-13 09:41:06 -0230

    more additions

--- public/js/index.js
+++ public/js/index.js
@@ -1,13 +1,17 @@
-
+//navbar snaping
 window.onscroll = function() {myFunction()};
   
 var navbar = document.getElementById("navbar");
-var sticky = navbar.offsetTop;
+var sticky = navbar.offsetTop-70;
   
 function myFunction() {
     if (window.pageYOffset >= sticky) {
       navbar.classList.add("sticky")
+      navbar.classList.remove("hidden");
     } else {
       navbar.classList.remove("sticky");
+      navbar.classList.add("hidden")
     }
   }
+
+  
