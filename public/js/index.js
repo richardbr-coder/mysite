7a79098e7b3465f2bf4c4650165fefcc92a1dabb
Richard Randell <richrdrandell2@outlook.com>
2020-10-21 10:59:37 -0230

    Chnages

--- public/js/index.js
+++ public/js/index.js
@@ -1,17 +1,85 @@
-//navbar snaping
-window.onscroll = function() {myFunction()};
+$(document).ready(function() {
+
+    // Transition effect for navbar and back-to-top icon
+    $(window).scroll(function() {
+      // checks if window is scrolled more than 500px, adds/removes solid class
+      if($(this).scrollTop() > 1100) { 
+          $('.navbar').addClass('solid');
+          $('.back-to-top').addClass('visible'); 
+      } else {
+          $('.navbar').removeClass('solid');
+          $('.back-to-top').removeClass('visible');  
+      }
+
+    });
+
+
+    // Scrolling effect for Arrow icons
+    $("#scrollIcon").click(function(e) {
+        e.preventDefault();
+        $.scrollTo($("#about"), 1000);
+    });
+    $("#nav-about").click(function(e) {
+        e.preventDefault();
+        $.scrollTo($("#about"), 1000);
+    });
+    $("#nav-portfolio").click(function(e) {
+        e.preventDefault();
+        $.scrollTo($("#portfolio"), 1000);
+    });
+    $("#nav-contact").click(function(e) {
+        e.preventDefault();
+        $.scrollTo($("#contact"), 1000);
+    });
+    $(".navbar-brand").click(function(e) {
+        e.preventDefault();
+        $.scrollTo(0, 1000);
+    });
+      
+  });
+
+
+//I am a developer typing
+  const typedTextSpan = document.querySelector(".typed-text");
+  const cursorSpan = document.querySelector(".cursor");
   
-var navbar = document.getElementById("navbar");
-var sticky = navbar.offsetTop-70;
+  const textArray = ["Developer", "Designer", "Programmer", "Photographer", "Drone Pilot", "Husband", "Dad"];
+  const typingDelay = 150;
+  const erasingDelay = 100;
+  const newTextDelay = 1000; // Delay between current and next text
+  let textArrayIndex = 0;
+  let charIndex = 0;
   
-function myFunction() {
-    if (window.pageYOffset >= sticky) {
-      navbar.classList.add("sticky")
-      navbar.classList.remove("hidden");
-    } else {
-      navbar.classList.remove("sticky");
-      navbar.classList.add("hidden")
+  function type() {
+    if (charIndex < textArray[textArrayIndex].length) {
+      if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+      typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+      charIndex++;
+      setTimeout(type, typingDelay);
+    } 
+    else {
+      cursorSpan.classList.remove("typing");
+        setTimeout(erase, newTextDelay);
     }
   }
+  
+  function erase() {
+      if (charIndex > 0) {
+      if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+      typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex-1);
+      charIndex--;
+      setTimeout(erase, erasingDelay);
+    } 
+    else {
+      cursorSpan.classList.remove("typing");
+      textArrayIndex++;
+      if(textArrayIndex>=textArray.length) textArrayIndex=0;
+      setTimeout(type, typingDelay + 1100);
+    }
+  }
+  
+  document.addEventListener("DOMContentLoaded", function() { // On DOM Load initiate the effect
+    if(textArray.length) setTimeout(type, newTextDelay + 250);
+  });
 
   
